09ae34be492b98e01654d921802f0d81ad149fba
shsazib <shsazibbhuiyan892939@gmail.com>
2022-12-02 22:06:17 -0800

    add content section and responsive manegment section

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Background from "./img/home-background.jpg";
-import SMang from './img/sel-mang.jpg';
-import StoMang from './img/sto_mang.jpg';
-import accoMang from './img/accou_mang.jpg';
-import repMang from './img/report.jpg';
+import SMang from "./img/sel-mang.png";
+import StoMang from "./img/sto_mang.png";
+import accoMang from "./img/accou_mang.png";
+import repMang from "./img/report.png";
 import Desktop from "./img/Desktop.png";
 import { NavLink } from "react-router-dom";
+import { RiArrowRightFill } from "react-icons/ri";
+import { BsYoutube, BsFacebook, BsWhatsapp } from "react-icons/bs";
 
 const home = () => {
   return (
@@ -31,16 +33,47 @@ const home = () => {
           </div>
         </div>
       </section>
+
+      <section className="contact_section">
+        <a href="" className="con_item con_item_whatsapp">
+          <BsWhatsapp className="con_item_social con_whatsapp" />
+          <div>
+            <h3>Whatsapp</h3>
+            <p>Messages</p>
+          </div>
+          <RiArrowRightFill />
+        </a>
+        <a href="" className="con_item con_item_facebook">
+          <BsFacebook className="con_item_social con_facebook" />
+          <div>
+            <h3>Facebook</h3>
+            <p>News Feed</p>
+          </div>
+          <RiArrowRightFill />
+        </a>
+        <a href="" className="con_item con_item_youtube">
+          <BsYoutube className="con_item_social con_youtube" />
+          <div>
+            <h3>Youtube</h3>
+            <p>Tutorial Videos</p>
+          </div>
+          <RiArrowRightFill />
+        </a>
+      </section>
+
       <section className="home_mang_section">
         <div className="home_mang">
           <div className="home_mang_con">
             <div>
               <h2>সেল’স ম্যানেজমেন্ট</h2>
-              <p>দৈনন্দিন কাস্টমারের তালিকা, বেচা-বিক্রি , মানি রিসিট প্রদান এবং বাকিতে লেন-দেনসহ সকল কাজ করা যাবে সহজেই।</p>
+              <p>
+                দৈনন্দিন কাস্টমারের তালিকা, বেচা-বিক্রি , মানি রিসিট প্রদান এবং
+                বাকিতে লেন-দেনসহ সকল কাজ করা যাবে সহজেই।
+              </p>
             </div>
             <img src={SMang} alt="SMang" />
           </div>
-          <div className="home_mang_con">
+          <div className="home_mang_con home_mang_con_warp">
             <img src={StoMang} alt="StoMang" />
             <div>
               <h2>স্টক ম্যানেজমেন্ট</h2>
@@ -50,15 +83,22 @@ const home = () => {
           <div className="home_mang_con">
             <div>
               <h2>আকাউন্ট’স ম্যানেজমেন্ট</h2>
-              <p>ব্যবসায়িক ছোট বড় সকল প্রকার লেন-দেনসহ লাভ-ক্ষতির হিসেব রাখা যায় একদম সহজে ।</p>
+              <p>
+                ব্যবসায়িক ছোট বড় সকল প্রকার লেন-দেনসহ লাভ-ক্ষতির হিসেব রাখা যায়
+                একদম সহজে ।
+              </p>
             </div>
             <img src={accoMang} alt="accoMang" />
           </div>
-          <div className="home_mang_con">
+          <div className="home_mang_con home_mang_con_warp">
             <img src={repMang} alt="repMang" />
             <div>
               <h2>রিপোর্ট</h2>
-              <p>ব্যবসায়িক সকল হিসাব এবং প্রতিনিয়ত আপডেট রাখতে পারবেন সহজেই। আপনার যেকোনো ডিভাইস থেকে যেকোনো সময় দেখতে পারবেন আপনার ব্যবসায়িক সেল’স সহ অন্যান্য রিপোর্ট।</p>
+              <p>
+                ব্যবসায়িক সকল হিসাব এবং প্রতিনিয়ত আপডেট রাখতে পারবেন সহজেই।
+                আপনার যেকোনো ডিভাইস থেকে যেকোনো সময় দেখতে পারবেন আপনার ব্যবসায়িক
+                সেল’স সহ অন্যান্য রিপোর্ট।
+              </p>
             </div>
           </div>
         </div>
