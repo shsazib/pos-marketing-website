5ad2ffec5a7cbbce4ef01778494f24a2f88a703f
shsazib <shsazibbhuiyan892939@gmail.com>
2022-12-05 23:11:24 -0800

    nav stuctstuct change and outside click nav display none add

--- src/Component/navbar.jsx
+++ src/Component/navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { NavLink, Link } from "react-router-dom";
 import Logo from "./img/logo.png";
 import { FaBars } from "react-icons/fa";
@@ -6,15 +6,22 @@ import { GrClose } from "react-icons/gr";
 import { GoTriangleDown } from "react-icons/go";
 
 const navbar = () => {
-  const [iconChange, setIconChange] = useState(true);
+  const [iconChange, setIconChange] = useState(false);
   const [subTogg, setSubTogg] = useState(false);
 
-  const IconClick = () => {
-    setIconChange(!iconChange);
-  };
-  const SubToggle = () => {
-    setSubTogg(!subTogg);
-  };
+  let menuRef = useRef();
+
+  useEffect(() => {
+    let handler = (e) => {
+      if (!menuRef.current.contains(e.target)) {
+        setIconChange(false);
+      }
+    };
+    document.addEventListener("mousedown", handler);
+    return () => {
+      document.removeEventListener("mousedown", handler);
+    };
+  });
 
   return (
     <>
@@ -67,62 +74,83 @@ const navbar = () => {
                 <NavLink to="/demo">ডেমো</NavLink>
               </li>
             </ul>
-            <div onClick={IconClick}>
-              {iconChange ? (
-                <FaBars className="nav_togg_btn" />
-              ) : (
-                <GrClose className="nav_togg_btn" />
-              )}
-            </div>
           </nav>
         </div>
 
-        <div className={iconChange ? "phone_nav_none" : "phone_nav"}>
-          <ul className="phone_nav_ul">
-            <li>
-              <NavLink to="/home">হোমপেজ</NavLink>
-            </li>
-            <li>
-              <NavLink to="/aboutus">আমাদের সম্পর্কে</NavLink>
-            </li>
-            <div className="phone_nav_submenu_warp">
-              <li className="phone_sub_menu_services">
-                <NavLink to={false} onClick={SubToggle}>
-                  সেবাসূমহ <GoTriangleDown />
-                </NavLink>
-                <ul
-                  className={subTogg ? "phone_sub_menu" : "phone_sub_menu_none"}
-                >
-                  <li>
-                    <a href="https://www.zealtechbd.com/" target="__blank">
-                      ওয়েব ডেভেলপমেন্ট
-                    </a>
-                  </li>
-                  <li>
-                    <a href="https://www.zealtechbd.com/" target="__blank">
-                      ডিজিটাল মার্কেটিং
-                    </a>
-                  </li>
-                  <li>
-                    <a href="https://www.zealtechbd.com/" target="__blank">
-                      ডোমেইন এবং হোস্টিং
-                    </a>
-                  </li>
-                  <li>
-                    <a href="https://www.zealtechbd.com/" target="__blank">
-                      গ্রাফিক্স ডিজাইন
-                    </a>
-                  </li>
-                </ul>
-              </li>
+        <div className="phone_nav_section" ref={menuRef}>
+          <div className="phone_nav_main">
+            <div className="nav_logo">
+              <Link to="/home">
+                <img src={Logo} alt="Logo"></img>
+              </Link>
             </div>
-            <li>
-              <NavLink to="/pricing">প্রাইসিং</NavLink>
-            </li>
-            <li>
-              <NavLink to="/demo">ডেমো</NavLink>
-            </li>
-          </ul>
+            <div
+              onClick={() => {
+                setIconChange(!iconChange);
+              }}
+            >
+              {iconChange ? (
+                <GrClose className="nav_togg_btn" />
+              ) : (
+                <FaBars className="nav_togg_btn" />
+              )}
+            </div>
+          </div>
+
+          <div className={iconChange ? "phone_nav" : "phone_nav_none"}>
+            <ul className="phone_nav_ul">
+              <li>
+                <NavLink to="/home">হোমপেজ</NavLink>
+              </li>
+              <li>
+                <NavLink to="/aboutus">আমাদের সম্পর্কে</NavLink>
+              </li>
+              <div className="phone_nav_submenu_warp">
+                <li className="phone_sub_menu_services">
+                  <NavLink
+                    to={false}
+                    onClick={() => {
+                      setSubTogg(!subTogg);
+                    }}
+                  >
+                    সেবাসূমহ <GoTriangleDown />
+                  </NavLink>
+                  <ul
+                    className={
+                      subTogg ? "phone_sub_menu" : "phone_sub_menu_none"
+                    }
+                  >
+                    <li>
+                      <a href="https://www.zealtechbd.com/" target="__blank">
+                        ওয়েব ডেভেলপমেন্ট
+                      </a>
+                    </li>
+                    <li>
+                      <a href="https://www.zealtechbd.com/" target="__blank">
+                        ডিজিটাল মার্কেটিং
+                      </a>
+                    </li>
+                    <li>
+                      <a href="https://www.zealtechbd.com/" target="__blank">
+                        ডোমেইন এবং হোস্টিং
+                      </a>
+                    </li>
+                    <li>
+                      <a href="https://www.zealtechbd.com/" target="__blank">
+                        গ্রাফিক্স ডিজাইন
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+              </div>
+              <li>
+                <NavLink to="/pricing">প্রাইসিং</NavLink>
+              </li>
+              <li>
+                <NavLink to="/demo">ডেমো</NavLink>
+              </li>
+            </ul>
+          </div>
         </div>
       </section>
     </>
