9fcce2e56f85bb21a7c98ec9d6e74b14e38f41a0
shsazib <shsazibbhuiyan892939@gmail.com>
2022-12-08 02:12:26 -0800

    CUSTOMAR REVIEW ADD

--- src/Component/customarRev.jsx
+++ src/Component/customarRev.jsx
@@ -7,6 +7,10 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 
+// Import img
+import maleImg from "./img/male-cus.webp";
+import femaleImg from "./img/female-cus.webp";
+
 // import required modules
 import { Autoplay, Pagination, Navigation } from "swiper";
 
@@ -30,13 +34,26 @@ const carousel = () => {
         <SwiperSlide>
           <div className="content">
             <p>
-              ''এই সফটওয়ারে দিয়ে দোকানের দৈনিক হিসাব-নিকাশ থেকে শুরু করে,
-              কাস্টমার এর তালিকা, ডিস্কাউন্ট অফার সব কিছুই সহজে করতে পারছি। এক
-              কথায় অসাধারণ একটি সফটওয়ারে।''
+              আমি প্রায় ২ বছর যাবত সফটওয়ারটি ব্যববহার করছি, আলহুমদুলিল্লাহ এই ২
+              বছরে আমার যত ধরনের সার্ভিস প্রয়োজন হয়েছে আমি সময় মত পেয়েছি। এই
+              প্রতিষ্ঠান এর সবাই খুবই আন্তরিক।
             </p>
-            <img src="https://picsum.photos/200/300" alt="Tst img" />
-            <h1>রাশেদ ভুঈয়া </h1>
-            <p>রেস্টুরেন্ট শপ, ঢাকা</p>
+            <img src={femaleImg} alt="femaleImg" />
+            <h1>তানিয়া সুলতানা</h1>
+            <p>হ্যান্ডিক্রাফ্ট শপ, ঢাকা</p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="content">
+            <p>
+              আমি যেখানেই থাকি না কেন, খুব ভাল মতোই আমার স্টক প্রোডাক্ট এর হিসাব
+              রাখতে পারছি। এখন আমাকে হিসাব রাখার জন্য ওয়্যারহাউজে যেতে হয় না।
+              তাই একজন হোলসেল ব্যবসায়ী হিসেবে আমি বলবো “আমার সলিউসন” সফটওয়ারটি
+              সত্যিই আমার ব্যবসাকে সহজ করে দিয়েছে।
+            </p>
+            <img src={maleImg} alt="maleImg" />
+            <h1>আবুল কালাম</h1>
+            <p>স্টক ব্যবসায়ী</p>
           </div>
         </SwiperSlide>
         <SwiperSlide>
@@ -46,19 +63,7 @@ const carousel = () => {
               এর তালিকা, ডিস্কাউন্ট অফার সব কিছুই সহজে করতে পারছি। এক কথায়
               অসাধারণ একটি সফটওয়ারে।
             </p>
-            <img src="https://picsum.photos/200/300" alt="Tst img" />
-            <h1>রাশেদ ভুঈয়া </h1>
-            <p>রেস্টুরেন্ট শপ, ঢাকা</p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="content">
-            <p>
-              এই সফটওয়ারে দিয়ে দোকানের দৈনিক হিসাব-নিকাশ থেকে শুরু করে, কাস্টমার
-              এর তালিকা, ডিস্কাউন্ট অফার সব কিছুই সহজে করতে পারছি। এক কথায়
-              অসাধারণ একটি সফটওয়ারে।
-            </p>
-            <img src="https://picsum.photos/200/300" alt="Tst img" />
+            <img src={maleImg} alt="maleImg" />
             <h1>রাশেদ ভুঈয়া </h1>
             <p>রেস্টুরেন্ট শপ, ঢাকা</p>
           </div>
